3ccf4c7e659f51d4d6d92d43eb6ecbd71ab42b47
yuvalHelman <yuvalasaf3@gmail.com>
2019-04-14 19:39:14 +0300

    files deleted

--- prep_data.py
+++ prep_data.py
@@ -6,23 +6,17 @@
 from mercariPriceData.InferSent.encoder.models import InferSent  # change folders
 import pandas as pd
 from pandas import DataFrame
-import os
-import pickle
-import testing_the_data as tests
 import torch
 import nltk
 import numpy as np
 import time
 from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
-import openpyxl as pxl
 
 
 #################################################
 # Global Variables:
 puredata = pd.DataFrame()
 #################################################
-
 
 
 ''' series_to_encode: a 'series' type to be transfered to vectors by infersent '''
@@ -45,10 +39,10 @@ def infersent_encoder(series_to_encode, batch_size_to_encode):
         infersent.build_vocab(sentences, tokenize=True)
         print("done build vocab")
     except Exception as e:
-        print('build vocab failed')
+        print('build vocab failed. Error occurred during InferSent encoding')
         print(e)
 
-    # Cant Encode all at once.. (not enough RAM) , so we need to do it in batches
+    # Can't Encode all at once.. (not enough RAM) , so we need to do it in batches
     full_embeddings = [list(np.zeros(4096))]
     end_index, start_index, embeddings = 0, 0, 0
     try:
@@ -64,7 +58,6 @@ def infersent_encoder(series_to_encode, batch_size_to_encode):
             start_index = end_index
             end_index = start_index + batch_size_to_encode
             full_embeddings = np.append(full_embeddings, embeddings, axis=0)
-            # full_embeddings = np.concatenate((full_embeddings, embeddings))
 
         # when end_index is bigger-equal to the length, do a last encoding manually
         end_index = len(sentences)
@@ -72,37 +65,37 @@ def infersent_encoder(series_to_encode, batch_size_to_encode):
             part_of_sentences = sentences[start_index:end_index].copy()
             embeddings = infersent.encode(part_of_sentences, tokenize=True)
             full_embeddings = np.append(full_embeddings, embeddings, axis=0)
-
     except Exception as e:
         print('encoding failed on part of list: ', start_index, end_index)
         print(e)
 
-    full_embeddings = full_embeddings[1:]
-    print(full_embeddings.shape)
+    full_embeddings = full_embeddings[1:] # Erase the zero vector
 
     return full_embeddings
 
-
+''' Combines the text columns of a row into a single string '''
 def string_of_all_text_columns(row):
-    new_string = ''
+    Combined_columns = ''
 
     if (not (row['name'] == 'not known')):
-        new_string = new_string + " " + row['name']
+        new_string = Combined_columns + " " + row['name']
 
     if (not (row['category_name'] == 'not known')):
-        new_string = new_string + " " + row['category_name']
+        new_string = Combined_columns + " " + row['category_name']
 
     if (not (row['brand_name'] == 'not known')):
-        new_string = new_string + " " + row['brand_name']
+        new_string = Combined_columns + " " + row['brand_name']
 
     if (not (row['item_description'] == 'not known')) and (not (row['item_description'] == 'No description yet')):
-        new_string = new_string + " " + row['item_description']
-
-    Combined_columns = new_string
+        new_string = Combined_columns + " " + row['item_description']
 
     return Combined_columns
 
 
+'''
+Creates a new column for the dataframe, with a concatenated string of all text columns 
+from each row in it.
+'''
 def combine_text_columns_into_one_later(data):
     ret_data = data.copy()
     ret_data.apply(lambda row: string_of_all_text_columns(row), axis=1)
@@ -112,12 +105,18 @@ def combine_text_columns_into_one_later(data):
     return ret_data
 
 
+'''
+Iterates on data[start_index:end_index] and creates a processed data.
+Combines InferSent, one-hot encodings and everything and then writes this data into a pickle file.
+'''
 def data_preprocessing(data, start_index, end_index):
     """
     A preprocessing of the data, using InferSent, One-Hot encodings and arranging NaN's etc.
-    Returns: a new DataFrame with only numerical data. column length: [ (4096*4) + 2 + 5
+    Returns: a new DataFrame with only numerical data. column length: (4096*4) + 2 + 5
     """
-    data = data.iloc[start_index:end_index].copy()  # TODO: erase that for doing for all data
+    __start = time.time()
+
+    data = data.iloc[start_index:end_index].copy()
 
     data.reset_index(inplace=True, drop=True)
 
@@ -172,37 +171,23 @@ def data_preprocessing(data, start_index, end_index):
 
 
     row_indices_list = range(start_index,end_index)
-    batch_size_to_encode = 5000 # TODO: for all data encoding
+    batch_size_to_encode = 5000 # Change for different size of batch to encode. (According to RAM constraints)
 
-    # encode the text column using Infersent.
-    # returns a numpy array of shape (rows, 4096)
+    # encode the text column using InferSent. returns a numpy array of shape (rows, 4096)
     description_embeddings = infersent_encoder(pd.Series(data["text_column"]), batch_size_to_encode)
     description_df = DataFrame.from_records(description_embeddings)
-    # description_df = pd.DataFrame( description_embeddings[0].reshape(-1, 4096) )
-    # for i in range(description_embeddings.shape[0]):
-    #     description_df.loc[i] = description_embeddings[i]
-    #
+
     data.drop(['text_column'], axis=1, inplace=True)
     data = pd.concat([data, description_df], axis=1)
 
-    # Write it to a pickle file # TODO: add this for more data generation
+    # Write it to a pickle file
     data.to_pickle('./output_pickle_{}_{}.pkl'.format(start_index, end_index), compression='bz2')
+
+    __end = time.time()
+    print("Time for total data preprocessing: ", __end - __start)
 
     return data
 
-
-def build_numerical_data(data):
-    start = time.time()
-
-    ''' preprocessing only a limited number of the data due to RAM constraints'''
-    start_index, end_index = 200000, 250000  # TODO: add this for more data generation
-    data_preprocessing(data, start_index, end_index)
-
-    ##### Save training data into a CSV (only on first batch):
-   # data.to_csv('./numeric_train_appended.csv', index=False, header=True)
-
-    end = time.time()
-    print("Time for total data preprocessing: ", end - start)
 
 
 ''' 
@@ -226,27 +211,27 @@ def get_reduced_data(data, reduced_dim=200):
 if __name__ == '__main__':
 
     '''fetching the data and transforming it into numerical data using Infersent '''
-    #puredata = pd.read_csv('./mercariPriceData/dataset/train.tsv', sep='\t', encoding="utf_8")  # change folders
-    # build_numerical_data(puredata)
+    #puredata = pd.read_csv('./mercariPriceData/dataset/train.tsv', sep='\t', encoding="utf_8")  # folders
+    ''' preprocessing only a limited number of the data due to RAM constraints'''
+    # start_index, end_index = 200000, 250000  # change this for more data generation
+    # data_preprocessing(puredata, start_index, end_index)
 
 
-    ''' Write the numeric data into a CSV, then use PCA on it and save to another CSV (split to test/train) '''
-    # df = pd.read_pickle('./output_pickle_0_15.pkl', compression='bz2')
-    # x_train, y_train = get_reduced_data(df, 100) # TODO: change to pickle
-    #
-    # x_train.to_pickle('x_train.pkl', compression='bz2')
-    # y_train.to_pickle('y_train.pkl', compression='bz2')
-
-    ''' Concatenate different files of processed data '''
-    # df = pd.read_pickle('./output_pickle_15_25.pkl', compression='bz2')
-    # x_test, y_test = get_reduced_data(df, 100)  # TODO: change to pickle
-    # x_test.to_pickle('x_test.pkl', compression='bz2')
-    # y_test.to_pickle('y_test.pkl', compression='bz2')
-
-
+    ''' Combining different processed pickle files into combined ones.'''
     #  Combining pickle files into ones.
     # df2 = pd.read_pickle('./output_pickle_25_40.pkl', compression='bz2')
     # df = pd.read_pickle('./output_pickle_200000_250000.pkl', compression='bz2')
     # comb_df = pd.concat([df, df2], ignore_index=True)
     # comb_df.to_pickle('./output_pickle_25_40.pkl', compression='bz2')
+
+    ''' Write the numeric data into a CSV, then use PCA on it and save to another CSV (split to test/train) '''
+    # df = pd.read_pickle('./output_pickle_0_15.pkl', compression='bz2')
+    # x_train, y_train = get_reduced_data(df, 100)
     #
+    # x_train.to_pickle('x_train.pkl', compression='bz2')
+    # y_train.to_pickle('y_train.pkl', compression='bz2')
+
+    ''' Concatenate different files of processed data '''
+    # df = pd.read_pickle('./output_pickle_15_25.p
+    # x_test.to_pickle('x_test.pkl', compression='bz2')
+    # y_test.to_pickle('y_test.pkl', compression='bz2')
--- testing_the_data.py
+++
@@ -1,7 +0,0 @@
-# A Project regarding the following Kaggle competition:
-# https://www.kaggle.com/c/mercari-price-suggestion-challenge
-
-# Submitted by Yuval Helman and Jakov Zingerman
-import numpy as np
-
-import pandas as pd
